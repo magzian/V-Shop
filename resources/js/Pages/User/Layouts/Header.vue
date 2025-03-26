0db817bbdbf3e945029a6c1af3a48804512e7a91
Ian Magonya <ianbenard2002@gmail.com>
2025-03-26 06:58:27 +0300

    added svg of the user on the header

--- resources/js/Pages/User/Layouts/Header.vue
+++ resources/js/Pages/User/Layouts/Header.vue
@@ -40,11 +40,15 @@ const auth = usePage().props.auth;
                     data-dropdown-placement="bottom"
                 >
                     <span class="sr-only">Open user menu</span>
-                    <img
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                    </svg>
+
+                    <!-- <img
                         class="w-8 h-8 rounded-full"
                         src="/docs/images/people/profile-picture-3.jpg"
                         alt="user photo"
-                    />
+                    /> -->
                 </button>
                 <div v-else>
                     <Link :href="route('login')" type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Login</Link>
